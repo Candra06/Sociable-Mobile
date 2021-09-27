dafb5fb0b94bae0ded3763587dbd7ff509fdb1ea
unknown <herisetyawan233@gmail.com>
2021-09-28 00:43:14 +0700

    fitur konten

---
+++ lib/Pages/konten/models/konten_repository.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
+import 'package:sociable/Pages/konten/models/konten_model.dart';
+import 'package:sociable/helper/network.dart';
+import 'package:sociable/helper/pref.dart';
+
+class KontenRepository {
+  Future<List<KontenModel>> ListKonten() async {
+    String token = await Pref.getToken();
+    http.Response res = await http.get(
+      Uri.parse(EndPoint.getArtikel),
+      headers: {
+        'Authorization': 'Bearer ' + token,
+        "Accept": "application/json"
+      },
+    );
+    if (res.statusCode == 200) {
+      var data = json.decode(res.body);
+      List<dynamic> list = data['data'];
+      print(list);
+      return list.map((e) => KontenModel.fromJson(e)).toList();
+    } else {
+      return [];
+    }
+  }
+}
