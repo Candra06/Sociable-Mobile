6b856b62d45a93ab4e04a86782e005662642d869
unknown <herisetyawan233@gmail.com>
2021-09-28 22:40:46 +0700

    sebelum pull

--- lib/helper/network.dart
+++ lib/helper/network.dart
@@ -3,6 +3,7 @@ class EndPoint {
   static final String api = server + 'api/';
   static final String login = api + 'login';
   static final String getArtikel = api + 'artikel';
+  static String detailKonten(var id) => api + 'artikel/$id';
   static final String listForum = api + 'forum/all';
   static final String addForum = api + 'forum/post';
   static String detailForum(var id) => api + 'forum/detail/$id';
